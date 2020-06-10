52a9e1a0aeb5696d6fbf15925ecfd74d3b70a561
Daniel Ottah <daniel.ottah@live.co.uk>
2020-06-10 01:46:47 -0600

    addded postgreSql environment and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+import os
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,10 +11,10 @@ import datetime
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.secret_key = 'dany'
 api = Api(app)
-
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
